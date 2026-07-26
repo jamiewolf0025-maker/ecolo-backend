896858b70c9e21b7dad8e0677c4ddd811c965877
jamiewolf0025-maker <jamiewolf0025@gmail.com>
2026-07-26 10:38:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+require("dotenv").config();
 
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors({
+  origin: "https://jamiewolf0025-maker.github.io"
+}));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
